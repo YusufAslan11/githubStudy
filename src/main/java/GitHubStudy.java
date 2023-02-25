b87701ab5a5b2155a43e0fdc3da7369bba31434e
Yusuf Aslan <ttyusuf18@gmail.com>
2023-02-25 18:47:23 +0300

    V2

--- src/main/java/GitHubStudy.java
+++ src/main/java/GitHubStudy.java
@@ -2,6 +2,9 @@ public class GitHubStudy {
 
     public static void main(String[] args) {
         System.out.println("Merhaba v1");
+        System.out.println("Dünya ");
+        System.out.println("stasfh deneme");
+        System.out.println("pop");
     }
 
 }
